38d8aee28ea23a5fff120bc0e6ac9ab5ba3cf9e6
ivan-user <username.web@protonmail.com>
2021-03-08 14:25:21 +0100

    Auth progress

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { logout } from '../services/auth';
 
- 
-const navbar = () => {
+
+const handleLogout = props => {
+  logout().then(() => {
+    props.setUser(null);
+  });
+};
+
+const navbar = props => {
+
   return (
     <nav className="nav-style">
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/addpet">Add a Pet</Link>
-        </li>
+        {props.user ? (
+          <>
+            <li><Link to="/addpet">Add a Pet</Link></li>
+            <li><Link to="/" onClick={ () => handleLogout(props)}>Logout</Link></li>
+          </>
+          ) : (
+          <>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/login">Login</Link></li>
+          </>
+        )}
       </ul>
     </nav>
   )
